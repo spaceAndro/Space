e67a4db8750e627f78e1a61d09b1e8e71215d5e8
Dnut0121 <127921103+Dnut0121@users.noreply.github.com>
2024-06-04 03:01:57 +0900

    Add files via upload

---
+++ SpaceProject/src/main/java/com/rubypaper/SpaceApplication.java
@@ -0,0 +1,13 @@
+package com.rubypaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceApplication.class, args);
+	}
+
+}
