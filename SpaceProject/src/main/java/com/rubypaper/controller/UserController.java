21994f572101041c7686457453a788d9bde09a0e
duke1327 <dr9766@naver.com>
2024-10-04 00:24:59 +0900

    update sample design, create logout, name introduce

--- SpaceProject/src/main/java/com/rubypaper/controller/UserController.java
+++ SpaceProject/src/main/java/com/rubypaper/controller/UserController.java
@@ -3,11 +3,15 @@ package com.rubypaper.controller;
 import com.rubypaper.dto.User;
 import com.rubypaper.service.UserService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,5 +37,29 @@ public class UserController {
         model.addAttribute("message", "내 정보 페이지");
         
         return "profile"; // profile.html 템플릿 반환
+    }
+    
+    @GetMapping("/index2")
+    public String das(Model model) {
+    	// 현재 로그인한 사용자 정보 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName(); // 사용자 이름 (아이디)
+
+        // 사용자 정보 가져오기
+        User user = userService.findByUsername(username);
+
+        // 모델에 사용자 정보 추가
+        model.addAttribute("user", user);
+        
+    	return "index2";
+    }
+    
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {    
+            new SecurityContextLogoutHandler().logout(request, response, auth);  // 로그아웃 처리
+        }
+        return "login";  // 로그아웃 후 로그인 페이지로 리다이렉트
     }
 }
