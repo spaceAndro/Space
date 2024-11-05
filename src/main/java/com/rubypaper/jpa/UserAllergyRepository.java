5f91f542712895d6ee8a627a4ff44fd5140226f8
duke1327 <dr9766@naver.com>
2024-11-05 17:17:47 +0900

    캘린더 버그 수정

--- src/main/java/com/rubypaper/jpa/UserAllergyRepository.java
+++ src/main/java/com/rubypaper/jpa/UserAllergyRepository.java
@@ -9,4 +9,7 @@ import com.rubypaper.dto.UserAllergy;
 @Repository
 public interface UserAllergyRepository extends JpaRepository<UserAllergy, Integer> {
     UserAllergy findByUser(User user);
+    UserAllergy findByUser_uSeq(Integer uSeq); // User의 uSeq로 알러지 정보 조회
+    
+    void deleteByUser(User user); // 사용자로 알러지 정보 삭제
 }
